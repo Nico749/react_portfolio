473f39a3081371b1f5f72fcf095cf304cd7a90a1
Nik <pasqualininic@gmail.com>
2026-06-02 19:45:10 +1000

    fixed icons alignment

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -5,8 +5,7 @@ import { FaJsSquare } from 'react-icons/fa'
 import { FaGithub } from 'react-icons/fa'
 import { FaGit } from 'react-icons/fa'
 import { DiMongodb } from 'react-icons/di'
-import { DiMysql } from 'react-icons/di'
-import { SiPandas, SiSequelize, SiLooker, SiMicrosoftsqlserver } from 'react-icons/si'
+import { SiPandas, SiSequelize, SiLooker } from 'react-icons/si'
 import { FaSnowflake } from "react-icons/fa";
 import { DiGoogleCloudPlatform } from "react-icons/di";
 import { SiGooglesearchconsole } from "react-icons/si";
@@ -23,29 +22,20 @@ export default function Icons() {
             <h1 id="skills">Skills</h1>
             <div id="icons">
                 <div className="grid-container">
-                    <div>
-                        <div className='centered-label'><SiLooker id="icon" color="white" size="4rem" /> Tableau</div>
-                        <div className='centered-label'><PiFileSql id="icon" color="#c73030" size="4rem" /> SQL</div>
-                        <div className='centered-label'><SiSequelize id="icon" color="#2069fa" size="4rem" /> Sequelize</div>
-
-                    </div>
-                    <div>
-                        <div className='centered-label'><DiMongodb id="icon" color="green" size="4rem" /> MongoDB</div>
-                        <div className='centered-label'><SiGoogletagmanager id="icon" color="blue" size="4rem" />Tag Manager</div>
-                        <div className='centered-label'><SiPandas id="icon" color="darkblue" size="4rem" />Python Pandas</div>
-                    </div>
-                    <div>
-                        <div className='centered-label'><FaNode id="icon" color="green" size="4rem" /> NodeJS</div>
-                        <div className='centered-label'><SiGoogleanalytics id="icon" color="orange" size="4rem" />Google Analytics </div>
-                        <div className='centered-label'><SiGooglesearchconsole id="icon" color="#ffcc00" size="4rem" />Google Search Console </div>
-                        <div className='centered-label'><FaGithub id="icon" color="white" size="4rem" /> GitHub</div>
-                    </div>
-                    <div>
-                        <div className='centered-label'><FaJsSquare id="icon" color="yellow" size="4rem" /> JS</div>
-                        <div className='centered-label'><FaSnowflake id="icon" color="#00ccff" size="4rem" /> Snowflake</div>
-                        <div className='centered-label'><DiGoogleCloudPlatform id="icon" color="#0066ff" size="4rem" /> Google Cloud</div>
-                        <div className='centered-label'><FaGit id="icon" color="white" size="4rem" /> Git</div>
-                    </div>
+                    <div className='centered-label'><SiLooker className="skill-icon" color="white" size="4rem" /><span>Tableau</span></div>
+                    <div className='centered-label'><PiFileSql className="skill-icon" color="#c73030" size="4rem" /><span>SQL</span></div>
+                    <div className='centered-label'><SiSequelize className="skill-icon" color="#2069fa" size="4rem" /><span>Sequelize</span></div>
+                    <div className='centered-label'><DiMongodb className="skill-icon" color="green" size="4rem" /><span>MongoDB</span></div>
+                    <div className='centered-label'><SiGoogletagmanager className="skill-icon" color="blue" size="4rem" /><span>Tag Manager</span></div>
+                    <div className='centered-label'><SiPandas className="skill-icon" color="darkblue" size="4rem" /><span>Python Pandas</span></div>
+                    <div className='centered-label'><FaNode className="skill-icon" color="green" size="4rem" /><span>NodeJS</span></div>
+                    <div className='centered-label'><SiGoogleanalytics className="skill-icon" color="orange" size="4rem" /><span>Google Analytics</span></div>
+                    <div className='centered-label'><SiGooglesearchconsole className="skill-icon" color="#ffcc00" size="4rem" /><span>Google Search Console</span></div>
+                    <div className='centered-label'><FaGithub className="skill-icon" color="white" size="4rem" /><span>GitHub</span></div>
+                    <div className='centered-label'><FaJsSquare className="skill-icon" color="yellow" size="4rem" /><span>JS</span></div>
+                    <div className='centered-label'><FaSnowflake className="skill-icon" color="#00ccff" size="4rem" /><span>Snowflake</span></div>
+                    <div className='centered-label'><DiGoogleCloudPlatform className="skill-icon" color="#0066ff" size="4rem" /><span>Google Cloud</span></div>
+                    <div className='centered-label'><FaGit className="skill-icon" color="white" size="4rem" /><span>Git</span></div>
                 </div>
             </div>
         </div>

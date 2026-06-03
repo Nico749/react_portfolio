cddf17c6c122824622afd6e91972adb4738520bf
Nik <pasqualininic@gmail.com>
2026-06-03 17:50:26 +1000

    menu style changes and removed unused files

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,141 +1,58 @@
 import React, { useState } from 'react';
 import '../styles/NavTabs.css';
 
-// style for mouse over and out is declared here 
+const navItems = [
+  { page: 'Home', href: '#home', label: 'Home' },
+  { page: 'About', href: '#about', label: 'About' },
+  { page: 'AnalyticsProjects', href: '#analytics-projects', label: 'Analytics Projects' },
+  { page: 'Projects', href: '#web-dev-projects', label: 'Web Dev Projects' },
+  { page: 'Contacts', href: '#contacts', label: 'Contacts' },
+];
+
 function NavTabs({ currentPage, handlePageChange }) {
   const [isActive, setisActive] = useState(false);
-  function MouseOver(event) {
-    event.target.style.background = '#1aaaed';
-    event.target.style.border = "solid #1ba0de 1px";
-    event.target.style.borderRadius = '0 0 10px 10px';
-    
-  }
-  function MouseOut(event){
-    event.target.style.background="#5b6362";
-    event.target.style.border = "none";
 
-  }
   return (
     <>
       <nav className="navbar" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
         
-          <a 
+          <button
+            type="button"
             onClick={() => {
               setisActive(!isActive);
             }}
-            role="button"
+            aria-label="Toggle navigation menu"
+            aria-expanded={isActive}
             className={`navbar-burger burger has-text-white ${isActive ? "is-active" : ""}`}
             
           >
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
-          </a>
+          </button>
         </div>
         <div
           
           className={`navbar-menu ${isActive ? "is-active" : ""}`}
         >
           <div className="navbar-start">
-            <ul ><li >
-            <a onMouseOver={MouseOver} onMouseOut={MouseOut}
-              href="#home"
-              data-analytics = "Home"
-              onClick={() => {handlePageChange('Home');
-                //make the mobile menu closing when clicking on the item
-                setisActive(!isActive);
-              }}
-              className={`${'navbar-item has-text-white'} currentPage === 'Home' 
-          ? 'nav-link active' 
-          : 'nav-link'  `}
-            >
-              Home
-            </a></li><li >
-            <a onMouseOver={MouseOver} onMouseOut={MouseOut}
-              href="#about"
-              data-analytics = "About"              
-              onClick={() => {
-                handlePageChange('About');
-                setisActive(!isActive);
-              //   window.dataLayer.push({     //NOT NEEDED IT ANYMORE SINCE CAN GRAB THAT PAGE WITH HISTORY CHANGE ON GTM
-              //     event: 'custom_page_view',
-              //     eventProps: {
-              //        page: window.location.hash
-              //     }
-              //   });
-              }
-            }              
-              className={`${'navbar-item has-text-white'} currentPage === 'About' 
-          ? 'nav-link active' 
-          : 'nav-link'`}
-            >
-              About
-            </a></li>
-            <li >
-            <a onMouseOver={MouseOver} onMouseOut={MouseOut}
-              href="#analytics-projects"
-              data-analytics = "AnalyticsProjects"
-              onClick={() => { 
-                handlePageChange('AnalyticsProjects');
-                setisActive(!isActive);
-                // window.dataLayer.push({
-                //   event: 'custom_page_view',
-                //   eventProps: {
-                //      page: window.location.hash
-                //   }
-                // });
-              }
-
-              }
-              className={`${'navbar-item has-text-white'} currentPage === 'AnalyticsProjects' 
-          ? 'nav-link active' 
-          : 'nav-link'`}
-            >
-              Analytics Projects
-            </a></li>
-            <li >
-            <a onMouseOver={MouseOver} onMouseOut={MouseOut}
-              href="#web-dev-projects"
-              data-analytics = 'Projects'
-              onClick={() => {
-                handlePageChange('Projects');
-                setisActive(!isActive);
-                // window.dataLayer.push({
-                //   event: 'custom_page_view',
-                //   eventProps: {
-                //      page: window.location.hash
-                //   }
-                // });
-              }
-              
-              }
-              className={`${'navbar-item has-text-white'} currentPage === 'Projects' 
-          ? 'nav-link active' 
-          : 'nav-link'`}
-            >
-              Web Dev Projects
-            </a></li><li >
-            <a onMouseOver={MouseOver} onMouseOut={MouseOut}
-              href="#contacts"
-              data-analytics = 'Contacts'
-              onClick={() => {
-                handlePageChange('Contacts');
-                setisActive(!isActive);
-                // window.dataLayer.push({
-                //   event: 'custom_page_view',
-                //   eventProps: {
-                //      page: window.location.hash
-                //   }
-                // });
-              }
-              }
-              className={`${'navbar-item has-text-white'} currentPage === 'Contacts' 
-          ? 'nav-link active' 
-          : 'nav-link'`}
-            >
-              Contacts
-            </a></li>
+            <ul>
+              {navItems.map(({ page, href, label }) => (
+                <li key={page}>
+                  <a
+                    href={href}
+                    data-analytics={page}
+                    onClick={() => {
+                      handlePageChange(page);
+                      setisActive(false);
+                    }}
+                    className={`navbar-item has-text-white nav-link ${currentPage === page ? 'active' : ''}`}
+                  >
+                    {label}
+                  </a>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
